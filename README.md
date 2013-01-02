be7270a12c7350107eccc47bf871abbfe763d175
Chase Arnold <chase4926@gmail.com>
2013-01-02 18:55:40 -0500

    Added 5 new statistics, updated readme

--- README.md
+++ README.md
@@ -5,6 +5,19 @@
 
 Tracks various statistics over the lifetime of a bukkit minecraft server and saves them in stats.txt
 
+## List of statistics tracked: ##
+
+-  Arrows Fired
+-  Blocks Broke
+-  Blocks Burned
+-  Blocks Placed
+-  Eggs Thrown
+-  Items Crafted
+-  Mobs Killed
+-  Player Deaths
+-  Players Joined
+-  Snowballs Thrown
+
 ## Licensing: ##
 
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
--- src/com/gmail/chase4926/stattracker/StatTrackerListener.java
+++ src/com/gmail/chase4926/stattracker/StatTrackerListener.java
@@ -1,19 +1,49 @@
 package com.gmail.chase4926.stattracker;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class StatTrackerListener implements Listener {
 	private StatTracker plugin = null;
 	
 	public StatTrackerListener(StatTracker p) {
 		plugin = p;
+	}
+	
+	@EventHandler
+	public void ArrowFireTracker(EntityShootBowEvent event) {
+		String name = "ArrowsFired";
+		if (plugin.getConfig().getBoolean(name)) {
+			plugin.set_value(name, plugin.get_value(name) + 1);
+		}
+	}
+	
+	@EventHandler
+	public void BlockBreakTracker(BlockBreakEvent event) {
+		String name = "BlocksBroke";
+		if (plugin.getConfig().getBoolean(name)) {
+			plugin.set_value(name, plugin.get_value(name) + 1);
+		}
+	}
+	
+	@EventHandler
+	public void BlockBurnTracker(BlockBurnEvent event) {
+		String name = "BlocksBurned";
+		if (plugin.getConfig().getBoolean(name)) {
+			plugin.set_value(name, plugin.get_value(name) + 1);
+		}
 	}
 	
 	@EventHandler
@@ -25,8 +55,8 @@ public class StatTrackerListener implements Listener {
 	}
 	
 	@EventHandler
-	public void BlockBreakTracker(BlockBreakEvent event) {
-		String name = "BlocksBroke";
+	public void EggThrowTracker(PlayerEggThrowEvent event) {
+		String name = "EggsThrown";
 		if (plugin.getConfig().getBoolean(name)) {
 			plugin.set_value(name, plugin.get_value(name) + 1);
 		}
@@ -51,10 +81,28 @@ public class StatTrackerListener implements Listener {
 	}
 	
 	@EventHandler
-	public void ArrowFireTracker(EntityShootBowEvent event) {
-		String name = "ArrowsFired";
+	public void PlayerDeathTracker(PlayerDeathEvent event) {
+		String name = "PlayerDeaths";
 		if (plugin.getConfig().getBoolean(name)) {
 			plugin.set_value(name, plugin.get_value(name) + 1);
+		}
+	}
+	
+	@EventHandler
+	public void PlayerJoinTracker(PlayerJoinEvent event) {
+		String name = "PlayersJoined";
+		if (plugin.getConfig().getBoolean(name)) {
+			plugin.set_value(name, plugin.get_value(name) + 1);
+		}
+	}
+	
+	@EventHandler
+	public void SnowballThrowTracker(ProjectileLaunchEvent event) {
+		if (event.getEntityType() == EntityType.SNOWBALL) {
+			String name = "SnowballsThrown";
+			if (plugin.getConfig().getBoolean(name)) {
+				plugin.set_value(name, plugin.get_value(name) + 1);
+			}
 		}
 	}
 }
