c6119dc6afa0a92c6338639c6ef25f2db63e3e5f
Chase Arnold <chase4926@gmail.com>
2013-01-03 19:53:53 -0500

    Updated sinatra app

--- sinatra_app/app.rb
+++ sinatra_app/app.rb
@@ -43,10 +43,41 @@ class FileData
   end
 end
 
+class String
+  def upcase?()
+    if self == self.upcase() then
+      return true
+    else
+      return false
+    end
+  end
+  
+  def downcase?()
+    return !upcase?()
+  end
+end
+
+
+def de_camelcase(string)
+  string = String.new(string)
+  index_array = Array.new()
+  string.length().times() do |i|
+    if i > 0 and string[i].upcase?() and string[i - 1].downcase?() then
+      index_array << i
+    end
+  end
+  i_add = 0
+  index_array.each() do |i|
+    string.insert(i + i_add, ' ')
+    i_add += 1
+  end
+  return string
+end
+
 def format_table_row(key, value, last=false)
   return <<-heredoc
   <tr>
-    <td><p align="left">#{key}</p>#{last ? '' : '<hr>'}</td>
+    <td><p align="left">#{de_camelcase(key) << ':'}</p>#{last ? '' : '<hr>'}</td>
     <td><p align="right">#{value}</p>#{last ? '' : '<hr>'}</td>
   </tr>
   heredoc
@@ -68,7 +99,6 @@ def get_formatted_block(filedata_instance, filename='stats.txt')
   filedata_instance.load_file(filename)
   return format_table_rows(filedata_instance.get_hash())
 end
-
 
 
 filedata = FileData.new()
